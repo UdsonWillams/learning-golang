74563f9972a12cf2309a3d591d9be6e2abdeaa8d
UdsonWillams <udson.willams@gmail.com>
2024-04-14 22:43:40 -0300

    starting exercices, lvl1, number 1

---
+++ exercicios-nv1/ex_1.go
@@ -0,0 +1,20 @@
+// Utilizando o operador curto de declaração, atribua estes valores
+// as variaveis com os identificadores: "x", "y" e "z"
+// 1: 42, 2: "James Bond", 3: true
+// Depois demonstre os valores nestas variaveis, com:
+// 1. Uma unica declaração print.
+// 2. Multiplas declarações print.
+package main
+
+import "fmt"
+
+func main() {
+	x := 42
+	y := "James Bond"
+	z := true
+	fmt.Println(x, y, z)
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}
+

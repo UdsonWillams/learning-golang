eb641ea667cb58d4ac18561fa3b593b6bb48a2ce
UdsonWillams <udson.willams@gmail.com>
2024-04-18 00:27:57 -0300

    the exercise number 7

---
+++ exercicios-nv3/ex_7.go
@@ -0,0 +1,16 @@
+// - Utilizando a solução anterior, adicione as opções else if e else.
+
+package main
+
+import "fmt"
+
+func main() {
+	im_tired := "ain"
+	if im_tired == "nnn" {
+		fmt.Println("buxim chei")
+	} else if im_tired == "uepa" {
+		fmt.Println("buxim seco")
+	} else {
+		fmt.Println("oppaa")
+	}
+}

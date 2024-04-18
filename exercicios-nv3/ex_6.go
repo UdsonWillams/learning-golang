31feae104fd9727ccaf529629789b0015c5b75ab
UdsonWillams <udson.willams@gmail.com>
2024-04-18 00:27:52 -0300

    the exercise number 6

---
+++ exercicios-nv3/ex_6.go
@@ -0,0 +1,12 @@
+// - Crie um programa que demonstre o funcionamento da declaração if.
+
+package main
+
+import "fmt"
+
+func main() {
+	im_tired := true
+	if im_tired {
+		fmt.Println("buxim chei")
+	}
+}

2de3fe8adb60273b7acbaac32b6b57d5f7488b26
UdsonWillams <udson.willams@gmail.com>
2024-04-18 11:18:08 -0300

    exercise 9 from lvl 3

---
+++ exercicios-nv3/ex_9.go
@@ -0,0 +1,19 @@
+// - Crie um programa que utilize a declaração switch,
+// onde o switch statement seja uma variável do tipo
+// string com identificador "esporteFavorito".
+
+package main
+
+import "fmt"
+
+func main() {
+	esporteFavorito := "mma"
+	switch esporteFavorito {
+	case "mma":
+		fmt.Println("alexi poatann")
+	case "boxe":
+		fmt.Println("myke tyysown")
+	case "e-sports":
+		fmt.Println("bi erre teu teu")
+	}
+}

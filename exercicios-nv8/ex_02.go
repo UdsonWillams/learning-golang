73bdbae8cd3553b0d028ea69c48bb252e59296b8
UdsonWillams <udson.willams@gmail.com>
2024-04-30 01:04:18 -0300

    exemplo 2 do nvl 8

---
+++ exercicios-nv8/ex_02.go
@@ -0,0 +1,48 @@
+// - Partindo do código abaixo, utilize unmarshal e demonstre os valores.
+//     - https://play.golang.org/p/b_UuCcZag9
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type jsontogo []struct {
+	First   string   `json:"First"`
+	Last    string   `json:"Last"`
+	Age     int      `json:"Age"`
+	Sayings []string `json:"Sayings"`
+}
+
+func main() {
+	s := `[{"First":"James","Last":"Bond","Age":32,"Sayings":["Shaken, not stirred","Youth is no guarantee of innovation","In his majesty's royal service"]},{"First":"Miss","Last":"Moneypenny","Age":27,"Sayings":["James, it is soo good to see you","Would you like me to take care of that for you, James?","I would really prefer to be a secret agent myself."]},{"First":"M","Last":"Hmmmm","Age":54,"Sayings":["Oh, James. You didn't.","Dear God, what has James done now?","Can someone please tell me where James Bond is?"]}]`
+	fmt.Println(s)
+
+	var resultado jsontogo
+
+	err := json.Unmarshal([]byte(s), &resultado)
+	if err != nil {
+		fmt.Println("deu zica mano!", err)
+	}
+
+	fmt.Println(resultado)
+	fmt.Println(resultado[1])
+	fmt.Println(resultado[1].Last)
+
+}
+
+// EU fiz dessa forma mas a ideia do exercicio era outro.
+
+// func main() {
+// 	s := `[{"First":"James","Last":"Bond","Age":32,"Sayings":["Shaken, not stirred","Youth is no guarantee of innovation","In his majesty's royal service"]},{"First":"Miss","Last":"Moneypenny","Age":27,"Sayings":["James, it is soo good to see you","Would you like me to take care of that for you, James?","I would really prefer to be a secret agent myself."]},{"First":"M","Last":"Hmmmm","Age":54,"Sayings":["Oh, James. You didn't.","Dear God, what has James done now?","Can someone please tell me where James Bond is?"]}]`
+// 	fmt.Println(s)
+// 	err := json.Unmarshal([]byte(s), &s)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+// 	fmt.Println(string(s))
+
+// 	// Unmarshal recebe bytes e um ponteiro, Nesse caso eu passei o proprio s.
+// 	// O exemplo da pasta cap_16_aplicações é bem legal de se ver também.
+// }

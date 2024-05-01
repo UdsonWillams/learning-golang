e6a5f3052bda8d3bd6c44256a966056de2f44acb
UdsonWillams <udson.willams@gmail.com>
2024-05-01 00:08:20 -0300

    exercicio 5 do nv9

---
+++ exercicios-nv9/ex_05.go
@@ -0,0 +1,37 @@
+// - Utilize atomic para consertar a condição de corrida do exercício #3.
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"sync/atomic"
+)
+
+func main() {
+
+	fmt.Println("CPUs:", runtime.NumCPU())
+
+	var contador int64
+	contador = 0
+	totaldegoroutines := 15
+
+	var wg sync.WaitGroup
+	wg.Add(totaldegoroutines)
+
+	for i := 0; i < totaldegoroutines; i++ {
+		go func() {
+			atomic.AddInt64(&contador, 1)
+			runtime.Gosched()
+			fmt.Println("Contador:\t", atomic.LoadInt64(&contador))
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+	fmt.Println("Valor final:", contador)
+
+}
+
+//  verificar race conditions
+//  go run -race exercicios-nv9/ex_03.go

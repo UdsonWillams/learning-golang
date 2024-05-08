dbf4c8402afbca74b02ee5c24a6e5d21d080353e
UdsonWillams <udson.willams@gmail.com>
2024-05-07 22:46:24 -0300

    exemplo pratico de canais

---
+++ exercicios_extras/canais_select.go
@@ -0,0 +1,42 @@
+func main() {
+
+	par := make(chan int)
+	ímpar := make(chan int)
+	quit := make(chan bool)
+
+	go mandaNúmeros(par, ímpar, quit)
+
+	receive(par, ímpar, quit)
+}
+
+func mandaNúmeros(par, ímpar chan int, quit chan bool) {
+	total := 100
+	for i := 0; i < total; i++ {
+		if i % 2 == 0 {
+			par <- i
+		} else {
+			ímpar <- i
+		}
+	}
+	close(par)
+	close(ímpar)
+	quit <- true
+}
+
+func receive(par, ímpar chan int, quit chan bool) {
+	for {
+		select {
+		case v := <-par: // ver se recebeu e pega seu valor
+			fmt.Println("O número", v, "é par.")
+		case v := <-ímpar: // ver se recebeu e pega seu valor
+			fmt.Println("O número", v, "é ímpar.")
+		case v, ok := <-quit:
+			if !ok {
+				fmt.Println("Deu zebra! Ó:", v)
+			} else {
+				fmt.Println("Encerrando. Recebemos:", v)
+			}
+			return
+		}
+	}
+}
